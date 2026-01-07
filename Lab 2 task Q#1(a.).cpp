0be1275c3bfa5774450d40c0eb150ba666dd85b6
toheedmalikuniversity-dotcom <toheedmalikuniversity@gmail.com>
2026-01-07 17:14:08 +0500

    Add files via upload

---
+++ Lab 2 task Q#1(a.).cpp
@@ -0,0 +1,35 @@
+// include iostream library for input/output operations
+
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+
+//print student ID card 
+
+cout << "-------------------------------------------------" << endl;
+cout << "\t" << endl;
+cout << "\t\tStudent ID Card\t\t" << endl;
+cout << "\t" << endl;
+cout << "-------------------------------------------------" << endl;
+
+//Get student information name, rollno, department, session.
+
+cout << "\t" << endl;
+cout << "Name:\t\t\tToheed Irfan" << endl;
+cout << "\t" << endl;
+cout << "Roll No:\t\t2025_SET_06" << endl;
+cout << "\t" << endl;
+cout << "Department:\t\tSoftware Engineering" << endl;
+cout << "\t" << endl;
+cout << "Session:\t\t2025-2029" << endl;
+cout << "\t" << endl;
+
+cout << "-----------------------------------------------" << endl;
+
+
+return 0//return 0 to indicate successful execution of the program.
+
+
+}
