887213cdcc19bd2686c249e3efa9046ea8d581f9
toheedmalikuniversity-dotcom <toheedmalikuniversity@gmail.com>
2026-01-07 22:16:16 +0500

    Add files via upload

---
+++ Lab 10 task Q#2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1, sum = 0;
+
+    while (i <= 10) {
+        sum += i;
+        i++;
+    }
+
+    cout << "Sum of first 10 natural numbers = " << sum << endl;
+
+    return 0;
+}
