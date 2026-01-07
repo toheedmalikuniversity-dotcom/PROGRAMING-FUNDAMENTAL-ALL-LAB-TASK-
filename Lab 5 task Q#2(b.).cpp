f8aadadffa7d669b0d4787fdffef59a18a41d420
toheedmalikuniversity-dotcom <toheedmalikuniversity@gmail.com>
2026-01-07 17:31:17 +0500

    Add files via upload

---
+++ Lab 5 task Q#2(b.).cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int marks;
+	int Assignment_Marks=5;
+	cout<<"Marks:";
+	cin>>marks;
+	marks+=Assignment_Marks;
+	cout<<"Marks after adding grace:"<<marks;
+	return 0;
+}
