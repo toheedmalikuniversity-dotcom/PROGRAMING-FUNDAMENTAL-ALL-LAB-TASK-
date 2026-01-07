b9d91337c246416d0d5d26aed1d0599723e1b44e
toheedmalikuniversity-dotcom <toheedmalikuniversity@gmail.com>
2026-01-07 17:43:39 +0500

    Add files via upload

---
+++ Lab 9 task Q#1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+
+int n;
+cout << "Enter the Number" << endl;
+cin >> n;
+cout << "=========" << endl;
+for(int i=1; i<=n; i++){
+cout <<i << endl;
+}
+return 0;
+}
