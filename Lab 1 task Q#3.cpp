2dbf09b5a94b5a7a25e1f5a855f1c484a99de3df
toheedmalikuniversity-dotcom <toheedmalikuniversity@gmail.com>
2026-01-07 17:13:20 +0500

    Add files via upload

---
+++ Lab 1 task Q#3.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  float a,b;
+
+       cout << "enter the first number" << endl;
+       
+  cin >> a;
+
+    cout << "enter the second number" << endl;
+    
+ cin >> b;
+
+int sum=a+b;
+       
+   int subtract = a-b ;
+   
+ int product = a*b;
+            
+     float division = a/b;
+
+    cout << "sum  ="<<sum << endl;
+    
+cout << "subtract  ="<<subtract << endl;
+
+  cout << "product  ="<<product << endl;
+    
+      cout << "division  ="<<division << endl;
+
+return 0;
+}
