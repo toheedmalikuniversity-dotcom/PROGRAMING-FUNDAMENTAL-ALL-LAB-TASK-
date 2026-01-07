76ea7d51e4feaa44a56dc9fccc83e2bc534c2911
toheedmalikuniversity-dotcom <toheedmalikuniversity@gmail.com>
2026-01-07 17:08:58 +0500

    Add files via upload

---
+++ Lab 1 task Q#2.cpp
@@ -0,0 +1,21 @@
+//header file input output stream 
+#include<iostream>
+using namespace std;
+//start main function of programing from hear
+int main()
+{
+//Print student card
+cout << "*************************************************" << endl;
+cout << "* Name:            Toheed Irfan                 *" << endl;
+cout << "* Roll Number:     2025_SET_06                  *" << endl;
+cout << "* Department:      Software Engineering         *" << endl;
+cout << "* Address:         Muzaffarabad                 *" << endl;
+cout << "* Hobby:           Playing Cricket              *" << endl;
+cout << "* Qualification:   FS.c                         *" << endl;
+cout << "* SSC Marks:       804                          *" << endl;
+cout << "* HSSC Marks:      697                          *" << endl;
+cout << "*************************************************" << endl;
+
+//end of program 
+return 0;
+}
